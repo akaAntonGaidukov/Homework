e92bd6359261d96aa337dcae9c49a21f4192c605
akaAntonGaidukov <gaidukov.nvt@gmail.com>
2021-05-19 15:13:17 +0300

    My lesson 1 home work
    by Anton Gaidukov

---
+++ GB_L1_T1.py
@@ -0,0 +1,13 @@
+# Поработайте с переменными, создайте несколько, выведите на экран.
+# Запросите у пользователя некоторые числа и строки и сохраните в переменные,
+# затем выведите на экран.
+
+argstr = " a string"
+argint = 1
+argflloat = 1.5
+
+print(f"Here are some data that is writen inside this program: {argstr} , {argint} , {argflloat}")
+
+userarg = input("Enter your data here: ")
+
+print(f"Your input was: {userarg}")
